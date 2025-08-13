54bfd6b5807fa15b584cfed4c12c3da90f438abb
Anisha254 <pottam222@gmail.com>
2025-08-13 10:37:26 +0530

    Add files via upload

---
+++ Assi_3_1.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+result = factorial(n)
+print(f'Factorial of {n} is: ',result)
---
+++ Assi_3_2.py
@@ -0,0 +1,8 @@
+import math
+n = float(input("Enter a number: "))
+sqrt_val = math.sqrt(n)
+log_val = math.log(n)
+sine_val = math.sin(n)
+print(f"Square root: ",sqrt_val)
+print(f"Logarithm: ",log_val)
+print(f"Sine: ",sine_val)
